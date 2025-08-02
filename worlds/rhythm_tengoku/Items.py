69644c008887dcd941567a20de7e21f59bef62e1
Th1nk <Tobiasransborg@hotmail.com>
2025-08-02 23:29:50 +0200

    Cleanup
    Refactoring slowly. Starting with Regions and Items

--- worlds/rhythm_tengoku/Items.py
+++ worlds/rhythm_tengoku/Items.py
@@ -1,69 +1,129 @@
-from __future__ import annotations
-from typing import ClassVar, Dict, List
+from typing import List, Dict, Set
+from dataclasses import dataclass
+from enum import Enum
 
-from BaseClasses import Item, ItemClassification
+base_id = 29300
 
+class ItemType(Enum):
+    Level = 0
+    Filler = 1
+    Trap = 2
 
-class RhythmTengokuItem(Item):
-    """Archipelago representation of a Rhythm Tengoku item."""
-    game: ClassVar[str] = "Rhythm Tengoku"
+@dataclass
+class RTItem:
+    name: str
+    type: ItemType
+    count: int = 1
 
+item_list: List[RTItem] = [
+    # Levels
+    RTItem("Karate Man", ItemType.Level),
+    RTItem("Karate Man 2", ItemType.Level),
+    RTItem("Clappy Trio", ItemType.Level),
+    RTItem("Snappy Trio", ItemType.Level),
+    RTItem("Polyrhythm", ItemType.Level),
+    RTItem("Polyrhythm 2", ItemType.Level),
+    RTItem("Night Walk", ItemType.Level),
+    RTItem("Night Walk 2", ItemType.Level),
+    RTItem("Rhythm Tweezers", ItemType.Level),
+    RTItem("Rhythm Tweezers 2", ItemType.Level),
+    RTItem("Sick Beats", ItemType.Level),
+    RTItem("Bouncy Road", ItemType.Level),
+    RTItem("Bouncy Road 2", ItemType.Level),
+    RTItem("Ninja Bodyguard", ItemType.Level),
+    RTItem("Ninja Reincarnate", ItemType.Level),
+    RTItem("Sneaky Spirits", ItemType.Level),
+    RTItem("Sneaky Spirits 2", ItemType.Level),
+    RTItem("Samurai Slice", ItemType.Level),
+    RTItem("Spaceball", ItemType.Level),
+    RTItem("Spaceball 2", ItemType.Level),
+    RTItem("Tap Trial", ItemType.Level),
+    RTItem("Tap Trial 2", ItemType.Level),
+    RTItem("Marching Orders", ItemType.Level),
+    RTItem("Marching Orders 2", ItemType.Level),
+    RTItem("Wizards Waltz", ItemType.Level),
+    RTItem("Bunny Hop", ItemType.Level),
+    RTItem("Fireworks", ItemType.Level),
+    RTItem("Power Calligraphy", ItemType.Level),
+    RTItem("Toss Boys", ItemType.Level),
+    RTItem("Toss Boys 2", ItemType.Level),
+    RTItem("Rat Race", ItemType.Level),
+    RTItem("Tram Pauline", ItemType.Level),
+    RTItem("Showtime", ItemType.Level),
+    RTItem("Space Dance", ItemType.Level),
+    RTItem("Cosmic Dance", ItemType.Level),
+    RTItem("Rap Men", ItemType.Level),
+    RTItem("Rap Women", ItemType.Level),
+    RTItem("Quiz Show", ItemType.Level),
+    RTItem("Bon Odori", ItemType.Level),
+    RTItem("Bon Dance", ItemType.Level),
+    RTItem("Remix 1", ItemType.Level),
+    RTItem("Remix 2", ItemType.Level),
+    RTItem("Remix 3", ItemType.Level),
+    RTItem("Remix 4", ItemType.Level),
+    RTItem("Remix 5", ItemType.Level),
+    RTItem("Remix 6", ItemType.Level),
+    RTItem("Remix 7", ItemType.Level),
+    RTItem("Remix 8", ItemType.Level),
 
-# List of all Rhythm Tengoku levels in order of their in-game IDs.
-level_table: List[str] = [
-    "Karate Man",
-    "Karate Man 2",
-    "Clappy Trio",
-    "Snappy Trio",
-    "Polyrhythm",
-    "Polyrhythm 2",
-    "Night Walk",
-    "Night Walk 2",
-    "Rhythm Tweezers",
-    "Rhythm Tweezers 2",
-    "Sick Beats",
-    "Bouncy Road",
-    "Bouncy Road 2",
-    "Ninja Bodyguard",
-    "Ninja Reincarnate",
-    "Sneaky Spirits",
-    "Sneaky Spirits 2",
-    "Samurai Slice",
-    "Spaceball",
-    "Spaceball 2",
-    "Tap Trial",
-    "Tap Trial 2",
-    "Marching Orders",
-    "Marching Orders 2",
-    "Wizards Waltz",
-    "Bunny Hop",
-    "Fireworks",
-    "Power Calligraphy",
-    "Power Calligraphy 2",
-    "Toss Boys",
-    "Toss Boys 2",
-    "Rat Race",
-    "Tram Pauline",
-    "Showtime",
-    "Space Dance",
-    "Cosmic Dance",
-    "Rap Men",
-    "Rap Women",
-    "Quiz Show",
-    "Bon Odori",
-    "Bon Dance",
-    "Remix 1",
-    "Remix 2",
-    "Remix 3",
-    "Remix 4",
-    "Remix 5",
-    "Remix 6",
-    "Remix 7",
-    "Remix 8",
-    "Blinking Trap",
-    "Beat Game",
+    # Filler
+    RTItem("RT Filler", ItemType.Filler),
+
+    # Traps
+    RTItem("Blinking Trap", ItemType.Trap)
 ]
 
-
-# Map each level name to its corresponding Archipelago item ID.
-item_table: Dict[str, int] = {name: idx for idx, name in enumerate(level_table)}
+group_dict: Dict[str, Set[str]] = {
+    "junk": {"RT Filler",
+             "Blinking Trap"},
+    "filler": {"RT Filler"},
+    "trap": {"Blinking Trap"},
+    "levels": {"Karate Man",
+               "Karate Man 2",
+               "Clappy Trio",
+               "Snappy Trio",
+               "Polyrhythm",
+               "Polyrhythm 2",
+               "Night Walk",
+               "Night Walk 2",
+               "Rhythm Tweezers",
+               "Rhythm Tweezers 2",
+               "Sick Beats",
+               "Bouncy Road",
+               "Bouncy Road 2",
+               "Ninja Bodyguard",
+               "Ninja Reincarnate",
+               "Sneaky Spirits",
+               "Sneaky Spirits 2",
+               "Samurai Slice",
+               "Spaceball",
+               "Spaceball 2",
+               "Tap Trial",
+               "Tap Trial 2",
+               "Marching Orders",
+               "Marching Orders 2",
+               "Wizards Waltz",
+               "Bunny Hop",
+               "Fireworks",
+               "Power Calligraphy",
+               "Toss Boys",
+               "Toss Boys 2",
+               "Rat Race",
+               "Tram Pauline",
+               "Showtime",
+               "Space Dance",
+               "Cosmic Dance",
+               "Rap Men",
+               "Rap Women",
+               "Quiz Show",
+               "Bon Odori",
+               "Bon Dance",
+               "Remix 1",
+               "Remix 2",
+               "Remix 3",
+               "Remix 4",
+               "Remix 5",
+               "Remix 6",
+               "Remix 7",
+               "Remix 8"},
+}
--- worlds/rhythm_tengoku/Regions.py
+++ worlds/rhythm_tengoku/Regions.py
@@ -1,20 +1,114 @@
-from __future__ import annotations
+from typing import List
+from dataclasses import dataclass
 
-from BaseClasses import MultiWorld, Region, Entrance
+@dataclass
+class RTRegion:
+    name: str
 
-from .Locations import RhythmTengokuLocation, location_table
+@dataclass
+class LevelRegion(RTRegion):
+    level_id: int
+    level_x: int
+    level_y: int
 
+class Regions:
+    l0 = LevelRegion("Karate Man", 0, 2, 11)
+    l1 = LevelRegion("Karate Man 2", 1, 8, 11)
 
-def create_regions(multiworld: MultiWorld, player: int) -> None:
-    """Create the basic world regions and link them."""
-    menu = Region("Menu", player, multiworld)
-    game_region = Region("Rhythm Tengoku", player, multiworld)
+    l2 = LevelRegion("Clappy Trio", 2, 2, 7)
+    l3 = LevelRegion("Snappy Trio", 3, 7, 11)
 
-    for name, loc_id in location_table.items():
-        game_region.locations.append(RhythmTengokuLocation(player, name, loc_id, game_region))
+    l4 = LevelRegion("Polyrhythm", 4, 5, 8)
+    l5 = LevelRegion("Polyrhythm 2", 5, 9, 9)
 
-    start = Entrance(player, "Start Game", menu)
-    menu.exits.append(start)
-    start.connect(game_region)
+    l6 = LevelRegion("Night Walk", 6, 5, 10)
+    l7 = LevelRegion("Night Walk 2", 7, 8, 8)
 
-    multiworld.regions.extend([menu, game_region])
+    l8 = LevelRegion("Rhythm Tweezers", 8, 2, 10)
+    l9 = LevelRegion("Rhythm Tweezers 2", 9, 8, 10)
+
+    l10 = LevelRegion("Sick Beats", 10, 3, 8)
+
+    l11 = LevelRegion("Bouncy Road", 11, 6, 11)
+    l12 = LevelRegion("Bouncy Road 2", 12, 9, 11)
+
+    l13 = LevelRegion("Ninja Bodyguard", 13, 6, 10)
+    l14 = LevelRegion("Ninja Reincarnate", 14, 8, 9)
+
+    l15 = LevelRegion("Sneaky Spirits", 15, 3, 11)
+    l16 = LevelRegion("Sneaky Spirits 2", 16, 9, 7)
+
+    l17 = LevelRegion("Samurai Slice", 17, 3, 10)
+
+    l18 = LevelRegion("Spaceball", 18, 2, 8)
+    l19 = LevelRegion("Spaceball 2", 19, 9, 8)
+
+    l20 = LevelRegion("Tap Trial", 20, 6, 7)
+    l21 = LevelRegion("Tap Trial 2", 21, 7, 7)
+
+    l22 = LevelRegion("Marching Orders", 22, 2, 9)
+    l23 = LevelRegion("Marching Orders 2", 23, 8, 7)
+
+    l24 = LevelRegion("Wizards Waltz", 24, 4, 11)
+
+    l25 = LevelRegion("Bunny Hop", 25, 4, 9)
+
+    l26 = LevelRegion("Fireworks", 26, 6, 8)
+
+    l27 = LevelRegion("Power Calligraphy", 27, 5, 9)
+
+    l28 = LevelRegion("Toss Boys", 28, 6, 9)
+    l29 = LevelRegion("Toss Boys 2", 29, 9, 10)
+
+    l30 = LevelRegion("Rat Race", 30, 3, 9)
+
+    l31 = LevelRegion("Tram Pauline", 31, 4, 8)
+
+    l32 = LevelRegion("Showtime", 32, 4, 10)
+
+    l33 = LevelRegion("Space Dance", 33, 4, 7)
+    l34 = LevelRegion("Cosmic Dance", 34, 7, 9)
+
+    l35 = LevelRegion("Rap Men", 35, 5, 7)
+    l36 = LevelRegion("Rap Women", 36, 7, 8)
+
+    l37 = LevelRegion("Quiz Show", 37, 5, 11)
+    
+    l38 = LevelRegion("Bon Odori", 38, 3, 7)
+    l39 = LevelRegion("Bon Dance", 39, 7, 10)
+
+    l40 = LevelRegion("Remix 1", 40, 2, 6)
+    l41 = LevelRegion("Remix 2", 41, 3, 6)
+    l42 = LevelRegion("Remix 3", 42, 4, 6)
+    l43 = LevelRegion("Remix 4", 43, 5, 6)
+    l44 = LevelRegion("Remix 5", 44, 6, 6)
+    l45 = LevelRegion("Remix 6", 45, 7, 6)
+    l46 = LevelRegion("Remix 7", 46, 8, 6)
+    l47 = LevelRegion("Remix 8", 47, 9, 6)
+
+    all_regions: List[RTRegion] = [
+        l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19,
+        l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, l30, l31, l32, l33, l34, l35, l36, l37, l38, l39,
+        l40, l41, l42, l43, l44, l45, l46, l47
+    ]
+
+    @classmethod
+    def get_from_name(cls, name: str) -> RTRegion:
+        for region in cls.all_regions:
+            if region.name == name:
+                return region
+        return None
+    
+    @classmethod
+    def get_from_id(cls, id: int) -> RTRegion:
+        for region in [region for region in cls.all_regions if isinstance(region, LevelRegion)]:
+            if region.level_id == id:
+                return region
+        return None
+
+    @classmethod
+    def get_from_coordinates(cls, x: int, y: int):
+        for region in [region for region in cls.all_regions if isinstance(region, LevelRegion)]:
+            if region.level_x == x and region.level_y == y:
+                return region
+        return None
